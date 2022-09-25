051c713eb19a24377c5a6cae28f07690a8e53f7a
Cresus83 <alan.thob@hotmail.fr>
2022-09-25 18:55:53 +0200

    removed useless css code for navbar

--- _navbar/navbar.php
+++ _navbar/navbar.php
@@ -1,5 +1,5 @@
 <!-- ======= Navbar ======= -->
-<header id="header" class="fixed-top">
+<header id="header">
         <div class="logo">
             <a href="#">
                 <img src="../assets/img/logo.png" alt="Logo-SUD'AIR"></a>
