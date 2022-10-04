2e2e87bf33f94630bc4258358abf00cbadab55a5
Karmaa83 <rayanhachanipro@gmail.com>
2022-10-04 17:42:03 +0200

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -207,13 +207,13 @@ function pollenData(pollendata) {
       return taxonsarray.some((item) => taxonstab[0].includes(item));
     }
     if (findEqualDatas(taxonsarray, taxonstab) == true) {
-      taxons[i].innerHTML = `<h4>PEUPLIER</h4>
+      taxons[i].innerHTML = `<h4>${taxons[i]}</h4>
       <img style="width: 100px;height: 100px" src="assets/img/pollen.png" alt="pollen-Symbole">
-      <h4 style='color:${couleurind};'>Présent</h4> `;
+      <div id="p${i}"><h4 style='color:${couleurind};'>Présent</h4></div> `;
     } else {
-      taxons[i].innerHTML = `<h4>PEUPLIER</h4>
+      taxons[i].innerHTML = `<h4>${taxons[i]}</h4>
       <img style="width: 100px;height: 100px" src="assets/img/pollen.png" alt="pollen-Symbole">
-      <h4 style='color:white;background-color:#AE0F0F;'>Indisponible</h4> `;
+      <div id="p${i}"><h4 style='color:white;background-color:#AE0F0F;'>Indisponible</h4></div>`;
     }
   }
 }
