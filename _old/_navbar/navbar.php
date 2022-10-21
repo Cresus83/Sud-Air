cbb1ac29d8a3b08a23e8102921f5fb1584ed461b
Cresus83 <alan.thob@hotmail.fr>
2022-10-21 17:52:39 +0200

    update accueil

--- _old/_navbar/navbar.php
+++
@@ -1,7 +0,0 @@
-<!-- ======= Navbar ======= -->
-<header id="header">
-        <div class="logo">
-            <a href="#">
-                <img src="../assets/img/logo.svg" alt="Logo-SUD'AIR"></a>
-        </div>
-</header><!-- Fin Navbar -->
