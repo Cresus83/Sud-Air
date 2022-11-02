e095aea09d2853eff995b972c90fbfd6ba7e80f1
Cresus83 <alan.thob@hotmail.fr>
2022-11-03 00:45:17 +0100

    add tips popup and some icons

--- assets/app/popup-conseil.js
+++ assets/app/popup-conseil.js
@@ -1,19 +1,6 @@
-(function(){
-  
-  // Defaults
-  var BUTTON = document.getElementById('btnpopup');
-  var POPUP = document.getElementById('mypopup');
-  var POPUP_CLASS = 'popup-ui';
-  
-  // Popup button (Click on button to open modal)
-  BUTTON.addEventListener('click', function(){
-    POPUP.className = POPUP_CLASS + ' show';
-  }, false);
+$(document).on('click', '.toggle', function(event) {
+  event.preventDefault();
 
-  // Popup modal (Click on modal to close it)
-  POPUP.addEventListener('click', function(){
-    this.className = POPUP_CLASS;
-  }, false);
-  
-  
-})();
+  var target = $(this).data('target');
+  $('#' + target).toggleClass('hide');
+});
