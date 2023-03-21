e69481e3a9034249c221f8f2842e32047a3a39be
Cresus83 <alan.thob@hotmail.fr>
2023-03-21 16:36:30 +0100

    Removed useless files

--- Rendu/assets/app/popup-conseil.js
+++
@@ -1,9 +0,0 @@
-$(document).on("click", ".toggle", function (event) {
-  event.preventDefault();
-  $(document.body).toggleClass("blockY");
-  $(document.body).removeClass("acceptY");
-  var target = $(this).data("target");
-  $("#" + target).toggleClass("hide");
-  $("#" + target).removeClass("blockY");
-  $("#" + target).toggleClass("acceptY");
-});
